614caa43b42b68b555af3236a0357a26d300eb18
Rudolf Luayon <103726548+dumpdope1@users.noreply.github.com>
2022-10-09 18:56:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -38,9 +38,19 @@ function updateDisplayYY(e) {
 function updateDisplayCVC(e) {
    displayCVC.innerHTML = e.target.value;
 }
+
 inputCardNumber.addEventListener('input', function (e) {
    var target = e.target, position = target.selectionEnd, length = target.value.length;
    
    target.value = target.value.replace(/[^\dA-Z]/g, '').replace(/(.{4})/g, '$1 ').trim();
    target.selectionEnd = position += ((target.value.charAt(position - 1) === ' ' && target.value.charAt(length - 1) === ' ' && length !== target.value.length) ? 1 : 0);
  });
+
+$(document).ready(function () {
+   $('.submit',);
+})
+
+$('.submit').on('click', function () {
+   $('.sucessfulConfirmation').show();
+   $('.PaymentInformation').hide();
+}) 
